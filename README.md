571924b2bbbd9b7a480df7dd08d4d3f96bdc0da2
Ballester <pedballester@gmail.com>
2015-12-10 01:46:52 -0200

    sent version

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # AnalisadorLexicoSintatico
-Analisador
+Analisador Lexico e Sintatico
+Funciona somente com gramaticas sem ambiguidade e recursão a esquerda
+Recursões a esquerda diretas são identificadas, indiretas não.
+Regras que derivam o vazio devem ser inseridas após a inserção de todas as outras menções a essa regra
 
 #Bibs
 sudo apt-get install python-networkx
 sudo apt-get install python-matplotlib
+sudo apt-get install python-pydot
--- __main__.py
+++ __main__.py
@@ -4,16 +4,19 @@ import matplotlib.pyplot as plt
 
 sin = Sintatico("inputs.in", debugging=True)
 plotter = False
+save_file = True
+first_rule = 'prog'
 
 edge_labels = {}
-
-#READ GRAMATICA FROM FILE
+#READ GRAMMAR FROM FILE
 fgrammar = open('gramatica.txt')
 fauto    = open('automato.txt')
 
 grammar = fgrammar.readlines()
 auto    = fauto.readlines()
 
+G = nx.DiGraph(selfloops=True, multiedges=True)
+G.add_node('prog', color='blue', style='filled')
 #Example: prog -> listcmd <EOF>
 #Nome da regra -> valores (entre <> sao terminais)
 for i in grammar:
@@ -28,6 +31,7 @@ for i in grammar:
             regra.append(('T', i[j][1:len(i[j])-1]))
         else:
             regra.append(('N', i[j]))
+            G.add_edge(i[0], i[j])
     sin.insereRegra(i[0], regra)
 
 #<-- Lista de estados -->
@@ -37,7 +41,7 @@ for i in grammar:
 #q0 q1 a-z
 #q0 q1 b
 #q0 q3 .
-G = nx.Graph()
+A = nx.DiGraph(selfloops=True, multiedges=True)
 read_estados = False
 read_trans   = False
 for i in auto:
@@ -60,13 +64,16 @@ for i in auto:
                 j = j[1:len(j)-1]
                 sin.insereEstado(j)
                 sin.setInicial(j)
+                A.add_node(j, color='red', style='filled')
             elif j[0] == '<':
                 j = j[1:len(j)-1]
                 sin.insereEstado(j, final=True)
+                A.add_node(j, color='blue', style='filled')
             else:
                 sin.insereEstado(j)
+                A.add_node(j)
 
-            G.add_node(j)
+            
     
     elif read_trans:
         i = i.split()
@@ -76,8 +83,8 @@ for i in auto:
             else:
                 sin.insereTrans(i[0], i[1], i[j])
 
-        G.add_edge(i[0], i[1])
-        edge_labels.update({(i[0], i[1]): i[j]})
+            A.add_edge(i[0], i[1])
+            edge_labels.update({(i[0], i[1]): i[j]})
 
 if plotter:
     node_size=300
@@ -90,17 +97,23 @@ if plotter:
     edge_text_pos=0.3
     text_font='sans-serif'
 
-    graph_pos=nx.spring_layout(G)
-    nx.draw_networkx_nodes(G,graph_pos,node_size=node_size, 
+    graph_pos=nx.shell_layout(A)
+    nx.draw_networkx_nodes(A,graph_pos,node_size=node_size, 
                                alpha=node_alpha, node_color=node_color)
-    nx.draw_networkx_edges(G,graph_pos,width=edge_tickness,
+    nx.draw_networkx_edges(A,graph_pos,width=edge_tickness,
                                alpha=edge_alpha,edge_color=edge_color)
-    nx.draw_networkx_labels(G, graph_pos,font_size=node_text_size,
+    nx.draw_networkx_labels(A, graph_pos,font_size=node_text_size,
                                 font_family=text_font)
-    nx.draw_networkx_edge_labels(G, graph_pos, edge_labels=edge_labels,
+    nx.draw_networkx_edge_labels(A, graph_pos, edge_labels=edge_labels,
                                 label_pos=edge_text_pos)
 
     plt.show()
+
+if save_file:
+    dot_a = nx.to_pydot(A)
+    dot_a.write_png('automato.png')
+    dot_g = nx.to_pydot(G)
+    dot_g.write_png('gramatica.png')
 
 #Palavras reservadas
 sin.insereReservada("if")
@@ -117,7 +130,7 @@ sin.insereDelimitador(" ")
 
 #Le do arquivo
 sin.le_token()
-if sin.executaRegra('prog'):
+if sin.executaRegra(first_rule):
     print 'Codigo correto'
 else:
     raise Exception('ERRO - Sintatico', sin.tk, sin.linha_atual+1, sin.col_atual+1)
